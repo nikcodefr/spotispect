f6e714e3319789ddcc1bc92c4495b8548031155b
nikhil <nikh.nam04@gmail.com>
2024-12-29 18:27:45 +0530

    feat: implemented app working

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from spotipy.oauth2 import SpotifyOAuth
 import streamlit as st
 import pandas as pd
 import os
+from dotenv import load_dotenv
 
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+load_dotenv()
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 REDIRECT_URI = 'http://localhost:5000'
 
 sp = spotipy.Spotify(
@@ -16,5 +19,23 @@ sp = spotipy.Spotify(
         scope='user-read-recently-played user-top-read user-read-private user-read-email user-follow-read playlist-read-private user-library-read'
     )
 )
+
+st.set_page_config(page_title='Spotify Song Analysis', page_icon=':musical_note:')
+st.title('Welcome')
+st.write('Analyze songs from your favorite artists')
+
+top_tracks = sp.current_user_top_tracks(limit=10, time_range='short_term')
+
+track_names = [track['name'] for track in top_tracks['items']]
+track_durations = [track['duration_ms'] / 60000 for track in top_tracks['items']]  
+
+df = pd.DataFrame({
+    'Track Name': track_names,
+    'Duration (Minutes)': track_durations
+})
+df.set_index('Track Name', inplace=True)
+
+st.subheader('Track Durations for Your Top Tracks')
+st.bar_chart(df, height=500)
 
 
